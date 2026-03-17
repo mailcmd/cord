99a2a4ddba3557be5c139c50e3e1875e33cac37e
Mauricio Santecchia <mauricios@vianet.com.ar>
2026-03-17 17:26:27 -0300

    .

---
+++ lib/cord/cord_httpserver.ex
@@ -0,0 +1,69 @@
+defmodule CORD.HTTPServer do
+  defmacro __using__(_opts) do
+    quote do
+      import Plug.Conn
+      import unquote(__MODULE__)
+      require Logger
+      
+      def init(options), do: options
+
+      @before_compile unquote(__MODULE__)       
+    end
+  end
+  
+
+  defmacro __before_compile__(_env) do
+    quote do
+      # Default response for non legal calls
+      def call(conn, _opts) do
+        Logger.log(:warning, "[CORD][HTTP] 404 - Try to get #{conn.request_path}")
+        conn
+        |> put_resp_content_type("text/plain")
+        |> send_resp(404, "404 Not Found!\n")
+      end
+    end
+  end
+
+  
+	defmacro get(path, do: block) do
+    quote do
+      def call(
+            %{method: "GET", request_path: unquote(path)} = var!(conn),
+            var!(opts)
+          ) do
+        unquote(block)
+      end      
+    end	  
+  end
+
+  ###################################################################################
+  # Utils 
+  ################################################################################### 
+  # def expand_html(html) do
+  #   html 
+  #   |> expand_for()
+  # end
+
+  # def expand_for(html) do
+  #   matchs = 
+  #     ~r/:foreach[\t ]+(.+?)[\t ]+in[\t ]+(.+?)[\t ]+:do(.+?):end/s
+  #     |> Regex.scan(html, capture: :all)
+
+  #   replaces = 
+  #     matchs
+  #     |> Enum.map(fn [_, r, rows, body] ->
+  #       """
+  #       ${#{rows}.map( __#{r}__ => { return `
+  #       #{body}
+  #       `}).join('')}
+  #       """
+  #       |> String.replace(":{#{r}.", "${__#{r}__.")
+  #     end)
+
+  #   matchs
+  #   |> Enum.with_index()
+  #   |> Enum.reduce(html, fn {[str, _, _, _], i}, acc ->
+  #     String.replace(acc, str, Enum.at(replaces, i))
+  #   end)
+  # end
+end
