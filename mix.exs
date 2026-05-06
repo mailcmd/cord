d74cf781ab66451b9227b47709d107e448140b72
Mauricio Santecchia <mailcmd@gmail.com>
2026-05-06 18:54:09 -0300

    .

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CORD.MixProject do
     [
       app: :cord,
       version: "0.1.1",
-      compilers:  [:pre_install] ++ Mix.compilers(),
+      compilers:  ["cmd scripts/install.sh"] ++ Mix.compilers(),
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
