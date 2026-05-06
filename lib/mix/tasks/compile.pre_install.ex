887c5ad0e42ea5516d1a60c7baeae53fd0303544
Mauricio Santecchia <mailcmd@gmail.com>
2026-05-06 18:45:44 -0300

    .

--- lib/mix/tasks/compile.pre_install.ex
+++ lib/mix/tasks/compile.pre_install.ex
@@ -3,16 +3,15 @@ defmodule Mix.Tasks.Compile.PreInstall do
 
   @impl Mix.Task.Compiler
   def run(_args) do
-    # Run your custom command here
+    Mix.shell().info("Running pre-install script...")
+    
     case System.cmd("sh", ["-c", "scripts/install.sh"]) do
       {_output, 0} ->
+        Mix.shell().info("Pre-install completed successfully")
         {:ok, []}
       {output, _exit_code} ->
         Mix.shell().error("Pre-install script failed: #{output}")
         {:error, [%Mix.Task.Compiler.Diagnostic{
-                   file: "scripts/pre_install.sh",
-                   position: 0,
-                   compiler_name: :pre_install,
                    message: "Pre-install failed",
                    severity: :error
                  }]}
