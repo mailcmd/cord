1c6bc918614f62a33c8d7a87072e5278f6cf8aeb
Mauricio Santecchia <mailcmd@gmail.com>
2026-05-06 12:08:23 -0300

    .

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule CORD.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :cord],
+      extra_applications: [:logger],
       mod: {CORD.Application, []}
     ]
   end
--- scripts/install.sh
+++ scripts/install.sh
@@ -7,6 +7,7 @@ mkdir lib/layout
 cp -R deps/cord/lib/layout/* lib/layout/
 
 mkdir -p config/local
+cp -R deps/cord/config/config.exs config/config.exs
 cp -R deps/cord/config/local.config.exs.example config/local/config.exs
 exit 0
 
