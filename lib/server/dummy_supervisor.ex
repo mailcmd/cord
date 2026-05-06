510cd1241c9059cc16e584f6a8a848b0e2fe98a3
Mauricio Santecchia <mailcmd@gmail.com>
2026-05-06 19:44:44 -0300

    .

---
+++ lib/server/dummy_supervisor.ex
@@ -0,0 +1,14 @@
+defmodule CORD.DummySupervisor do
+	use Supervisor
+
+  def start_link(_) do
+    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  @impl true
+  def init(_) do
+    children = [ ]
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+  
+end
